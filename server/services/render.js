a1bcfd286e93ec28f39bd81e8107bbd94533a6f6
salmento <salmentochitlango@gmail.com>
2021-04-25 14:09:24 -0400

    Setup http verbs

--- server/services/render.js
+++ server/services/render.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://localhost:5000/api/users')
+    // eslint-disable-next-line no-undef
+    axios.get(`http://localhost:${process.env.PORT || 8080}/api/users`)
         .then(function (response) {
             res.render('index', { users: response.data });
         })
@@ -18,7 +19,8 @@ exports.add_user = (req, res) => {
 }
 
 exports.update_user = (req, res) => {
-    axios.get('http://localhost:5000/api/users', { params: { id: req.query.id } })
+    // eslint-disable-next-line no-undef
+    axios.get(`http://localhost:${process.env.PORT || 8080}/api/users`, { params: { id: req.query.id } })
         .then(function (userdata) {
             res.render('update_user', { user: userdata.data })
         })
