d5ac23132578276f2e250e118720df686a23d139
salmento <salmentochitlango@gmail.com>
2021-04-25 14:08:53 -0400

    Setup http verbs

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try{
         // mongodb connection string
+        // eslint-disable-next-line no-undef
         const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
@@ -13,6 +14,7 @@ const connectDB = async () => {
         console.log(`MongoDB connected : ${con.connection.host}`);
     }catch(err){
         console.log(err);
+        // eslint-disable-next-line no-undef
         process.exit(1);
     }
 }
